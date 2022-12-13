695da2f8facd725897045c085cdf00d55e4fadf1
Krisztian2 Kovacs <krisztian2.kovacs.ext@siemens-healthineers.com>
2022-12-13 16:06:17 +0100

    commit 3

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
-import '../../node_modules/draft-js/dist/Draft.css';
-import '../styles/MyEditor.css';
-import Toolbar from './Toolbar';
+import React, { useEffect, useRef, useState } from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
+import "../../node_modules/draft-js/dist/Draft.css";
+import "../styles/MyEditor.css";
+import Toolbar from "./Toolbar";
+import { saveEditorState } from "../utils";
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
@@ -11,21 +12,35 @@ function MyEditor() {
   const domEditor = useRef();
 
   useEffect(() => {
-    domEditor.current.focus();
+    focusToEditor();
   }, []);
 
+  function focusToEditor() {
+    domEditor.current.focus();
+  }
+
+  const styleMap = {
+    HIGHLIGHT: {
+      backgroundColor: "yellow",
+    },
+  };
+
   return (
-    <div className='editor-container'>
-      <Toolbar
-        editorState={editorState}
-        setEditorState={setEditorState}
-      />
-      <div className='editor'>
+    <div className="editor-container">
+      <div className="toolbar">
+        <Toolbar
+          editorState={editorState}
+          setEditorState={setEditorState}
+          focusToEditor={focusToEditor}
+        />
+      </div>
+      <div className="editor" onClick={focusToEditor}>
         <Editor
           editorState={editorState}
           onChange={setEditorState}
-          placeholder={'Enter text...'}
+          placeholder="Write something..."
           ref={domEditor}
+          customStyleMap={styleMap}
         />
       </div>
     </div>
